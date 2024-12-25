1696d32322284b625e5804fce2205d504179244f
shouta0715 <Myheroes0715@gmail.com>
2024-12-25 16:34:52 +0900

    Initial Commit

---
+++ App/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by k21047kk on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Landmarks
+//
+//  Created by k21047kk on 2024/12/25.
+//
+
